6bfc90e9179be1b802df5be95321014ce22c51d8
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-03-15 15:36:01 +0530

    Create Slote Table

---
+++ Node/Tables/Create_Slote_Table.js
@@ -0,0 +1,22 @@
+const { mySqlConnectionPool } = require("../Config/Db");
+
+const createSloteTable = async () => {
+
+    try {
+        
+        const createSloteTable = await mySqlConnectionPool.query("CREATE TABLE IF NOT EXISTS slote (slote_id INT AUTO_INCREMENT PRIMARY KEY, date DATE NOT NULL, start_time TIME NOT NULL, end_time TIME NOT NULL, status ENUM('AVAILABLE', 'BOOKED') NOT NULL DEFAULT 'AVAILABLE', turf_id INT, FOREIGN KEY (turf_id) REFERENCES turf(TURF_ID))");
+
+        console.log(`Slote Table Created Successfully`.bgGreen);
+
+    } catch (error) {
+        
+        console.log(`Error in creating Slote table : ${error.message}`.bgRed);     
+    }
+}
+
+createSloteTable();
+
+
+
+
+
